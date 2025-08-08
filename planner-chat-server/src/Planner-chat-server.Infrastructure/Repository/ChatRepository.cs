953e2bf44ba78c3185d59566e2b357c0b724d524
Даниил Пустовалов <pustden524@gmail.com>
2025-08-08 19:05:49 +0500

    фикс чата

--- planner-chat-server/src/Planner-chat-server.Infrastructure/Repository/ChatRepository.cs
+++ planner-chat-server/src/Planner-chat-server.Infrastructure/Repository/ChatRepository.cs
@@ -233,8 +233,8 @@ namespace Planner_chat_server.Infrastructure.Repository
 
         public async Task<List<ChatMessage>> GetAllMessagesAsync(Guid accountId)
         {
-            var query = _context.ChatMemberships.Include(c => c.Chat).Where(cm => cm.AccountId == accountId).Select(c => c.Chat);
-            var messages = query.Include(x => x.Messages).SelectMany(x => x.Messages);
+            var query = _context.ChatMemberships.Include(c => c.Chat).ThenInclude(x => x.Messages).Where(cm => cm.AccountId == accountId).Select(c => c.Chat);
+            var messages = query.SelectMany(x => x.Messages);
 
             return await messages
                 .ToListAsync();
