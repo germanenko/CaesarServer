d0bf7f27970fda9857e16080b5be4e91f43cf643
Даниил Пустовалов <pustden524@gmail.com>
2025-08-15 16:41:22 +0500

    обновление колонки

--- planner-task-board/src/Planer_task_board.Infrastructure/Repository/TaskRepository.cs
+++ planner-task-board/src/Planer_task_board.Infrastructure/Repository/TaskRepository.cs
@@ -249,11 +249,11 @@ namespace Planer_task_board.Infrastructure.Repository
             task.StartDate = startDate?.ToUniversalTime();
             task.EndDate = endDate;
 
-            //var boardColumnTask = await _context.BoardColumnTasks.FirstOrDefaultAsync(x => x.TaskId == task.Id);
-            //if (boardColumnTask.ColumnId != columnId)
-            //{
-            //    boardColumnTask.ColumnId = columnId;
-            //}
+            var boardColumnTask = await _context.BoardColumnTasks.FirstOrDefaultAsync(x => x.TaskId == task.Id);
+            if (boardColumnTask.ColumnId != columnId)
+            {
+                boardColumnTask.ColumnId = columnId;
+            }
 
             await _context.SaveChangesAsync();
 
