7b104632ddfb5bb30a252025499964f8689fc830
halloman16 <pustden524@gmail.com>
2025-09-05 09:51:50 +0300

    Добавление базовой модели

---
+++ planner-task-board/src/Planer_task_board.Core/Entities/Models/ModelBase.cs
@@ -0,0 +1,8 @@
+namespace Planer_task_board.Core.Entities.Models
+{
+    public class ModelBase
+    {
+        public Guid Id { get; set; }
+        public DateTime ChangeDate { get; set; }
+    }
+}
