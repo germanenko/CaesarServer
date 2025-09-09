95d08d9a05d8167790640332ee2728ae8581d589
pblcenok <pustden524@gmail.com>
2025-09-09 19:14:40 +0500

    тест

--- planner-chat-server/src/Planner-chat-server.Api/Controllers/ChatController.cs
+++ planner-chat-server/src/Planner-chat-server.Api/Controllers/ChatController.cs
@@ -42,9 +42,10 @@ namespace Planner_chat_server.Api.Controllers
             await _chatService.ConnectToChat(tokenPayload.AccountId, chatId, ws, tokenPayload.SessionId);
         }
 
-        [HttpPost("api/sendFromEmail")]
-        [LocalOnly]
+
         [SwaggerOperation("Дублирование письма в чат")]
+        [LocalOnly]
+        [HttpPost("api/sendFromEmail")]
         [SwaggerResponse(200, Type = typeof(Guid))]
         [SwaggerResponse(404)]
         public async Task<IActionResult> SendMessageFromEmail(
