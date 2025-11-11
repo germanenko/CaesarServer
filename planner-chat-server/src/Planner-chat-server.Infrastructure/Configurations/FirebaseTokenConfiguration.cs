bb26793d88faddd903f5e7a3e84bf48853a85a8d
pblcenok <pustden524@gmail.com>
2025-11-11 13:54:05 +0500

    добавлен конфиг

---
+++ planner-chat-server/src/Planner-chat-server.Infrastructure/Configurations/FirebaseTokenConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Planner_chat_server.Core.Entities.Models;
+
+namespace Planner_chat_server.Infrastructure.Configurations
+{
+    public class AccountConfiguration : IEntityTypeConfiguration<FirebaseToken>
+    {
+        public void Configure(EntityTypeBuilder<FirebaseToken> builder)
+        {
+            builder.HasKey(e => new { e.UserId, e.Token });
+
+            builder.HasIndex(e => e.UserId);
+            builder.HasIndex(e => e.Token);
+
+            builder.Property(e => e.UserId).IsRequired();
+            builder.Property(e => e.Token).IsRequired().HasMaxLength(500);
+        }
+    }
+}
