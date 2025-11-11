7c98d142685fe7147c5fae5fed04b9535b5b6a35
pblcenok <pustden524@gmail.com>
2025-11-11 09:51:27 +0500

    убрана ошибка

--- planner-chat-server/src/Planner-chat-server.Api/Program.cs
+++ planner-chat-server/src/Planner-chat-server.Api/Program.cs
@@ -94,7 +94,6 @@ void ConfigureServices(IServiceCollection services)
 
     services.AddSingleton<IJwtService, JwtService>();
     services.AddSingleton<IChatConnectionService, ChatConnectionService>();
-    services.AddSingleton<IUserService, UserService>();
 
     services.AddSingleton<IFirebaseService, FirebaseService>(sp =>
         new FirebaseService(
