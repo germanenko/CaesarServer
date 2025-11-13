950f1365e958a6bee24d3806bd488b2140d2bb1e
pblcenok <pustden524@gmail.com>
2025-11-13 11:12:21 +0500

    Update LocalOnlyAttribute.cs

--- planner-notify-service/src/planner-notify-service.Api/CustomAttributes/LocalOnlyAttribute.cs
+++ planner-notify-service/src/planner-notify-service.Api/CustomAttributes/LocalOnlyAttribute.cs
@@ -12,10 +12,7 @@ namespace planner_notify_service.Api.CustomAttributes
             var requestHost = context.HttpContext.Request.Host.Host;
             var allowedHosts = new[] { "127.0.0.1", "planner-chat-server", "planner-notify-service" };
 
-            var remoteIp = context.HttpContext.Connection.RemoteIpAddress;
-            var localIp = context.HttpContext.Connection.LocalIpAddress;
-
-            if (!IPAddress.IsLoopback(remoteIp) && !remoteIp.Equals(localIp))
+            if (!allowedHosts.Contains(requestHost))
             {
                 context.Result = new ForbidResult();
             }
