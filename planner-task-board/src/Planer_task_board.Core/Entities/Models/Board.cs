d6d800662fe1d3ef0425d5d93134125d3b02b48c
pblcenok <pustden524@gmail.com>
2025-11-20 13:30:07 +0500

    Update Board.cs

--- planner-task-board/src/Planer_task_board.Core/Entities/Models/Board.cs
+++ planner-task-board/src/Planer_task_board.Core/Entities/Models/Board.cs
@@ -5,7 +5,6 @@ namespace Planer_task_board.Core.Entities.Models
     public class Board : ModelBase
     { 
         public string Name { get; set; }
-        public List<BoardColumn> Columns { get; set; } = new();
 
         public BoardBody ToBoardBody()
         {
