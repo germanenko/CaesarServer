621c09f2719a6c3ca873938ff3f308c350a83adc
pblcenok <pustden524@gmail.com>
2025-11-21 10:35:13 +0500

    откат

--- planner-task-board/src/Planer_task_board.Infrastructure/Repository/NodeRepository.cs
+++ planner-task-board/src/Planer_task_board.Infrastructure/Repository/NodeRepository.cs
@@ -1,5 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using Npgsql;
 using Planer_task_board.Core.Entities.Models;
 using Planer_task_board.Core.Enums;
 using Planer_task_board.Core.IRepository;
@@ -50,51 +49,11 @@ namespace Planer_task_board.Infrastructure.Repository
 
         public async Task<IEnumerable<Node>?> GetNodes(Guid accountId)
         {
-            var accessibleResourceIds = await _context.AccessRights
-                .Where(x => x.AccountId == accountId)
-                .Select(x => x.ResourceId)
-                .ToListAsync();
+            var access = await _context.AccessRights.Where(x => x.AccountId == accountId).Select(x => x.ResourceId).ToListAsync();
 
-            if (!accessibleResourceIds.Any())
-                return Enumerable.Empty<Node>();
+            var nodes = await _context.Nodes.Where(x => access.Contains(x.ParentId)).ToListAsync();
 
-            var allNodes = await _context.Nodes.ToListAsync();
-            var resultNodes = new HashSet<Node>();
-            var nodesToProcess = new Queue<Guid>(accessibleResourceIds);
-
-            while (nodesToProcess.Count > 0)
-            {
-                var currentNodeId = nodesToProcess.Dequeue();
-                var currentNode = allNodes.FirstOrDefault(x => x.Id == currentNodeId);
-
-                if (currentNode != null && resultNodes.Add(currentNode))
-                {
-                    var childrenIds = allNodes
-                        .Where(x => x.ParentId == currentNodeId)
-                        .Select(x => x.Id);
-
-                    foreach (var childId in childrenIds)
-                    {
-                        nodesToProcess.Enqueue(childId);
-                    }
-                }
-            }
-
-            return resultNodes.ToList();
-
-            //var accessibleResourceIds = await _context.AccessRights
-            //    .Where(x => x.AccountId == accountId)
-            //    .Select(x => x.ResourceId)
-            //    .ToListAsync();
-
-            //var nodes = await _context.Nodes
-            //.Where(x => accessibleResourceIds.Contains(x.ParentId) ||
-            //       accessibleResourceIds.Contains(x.Id))
-            //.ToListAsync();
-
-            //var nodes = await _context.Nodes.Where(x => accessibleResourceIds.Contains(x.ParentId)).ToListAsync();
-
-            //return nodes;
+            return nodes;
         }
     }
 }
