44178ade5bdca1ce369e1fc5c5aeecb42529ab42
pblcenok <pustden524@gmail.com>
2025-11-26 09:05:56 +0500

    исправлены тела создания досок и колонок

--- planner-task-board/src/Planer_task_board.Core/Entities/Request/CreateBoardBody.cs
+++ planner-task-board/src/Planer_task_board.Core/Entities/Request/CreateBoardBody.cs
@@ -3,9 +3,10 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Planer_task_board.Core.Entities.Request
 {
-    public class CreateBoardBody : ModelBase
+    public class CreateBoardBody
     {
-        [Required]
-        public string Name { get; set; }
+        [Required] public Guid Id { get; set; }
+        [Required] public string Name { get; set; }
+        [Required] public DateTime UpdatedAt { get; set; }
     }
 }
--- planner-task-board/src/Planer_task_board.Core/Entities/Request/CreateColumnBody.cs
+++ planner-task-board/src/Planer_task_board.Core/Entities/Request/CreateColumnBody.cs
@@ -3,9 +3,10 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Planer_task_board.Core.Entities.Request
 {
-    public class CreateColumnBody : ModelBase
+    public class CreateColumnBody
     {
-        [Required]
-        public string Name { get; set; }
+        [Required] public Guid Id { get; set; }
+        [Required] public string Name { get; set; }
+        [Required] public DateTime UpdatedAt { get; set; }
     }
 }
