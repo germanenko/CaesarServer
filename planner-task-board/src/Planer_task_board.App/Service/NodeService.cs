f95e1585cedaf5f5abe67a710e04a890f098f081
pblcenok <pustden524@gmail.com>
2025-12-03 13:47:33 +0500

    возвращение пустого списка ссылок

--- planner-task-board/src/Planer_task_board.App/Service/NodeService.cs
+++ planner-task-board/src/Planer_task_board.App/Service/NodeService.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Planer_task_board.App.Service
 {
@@ -43,7 +44,10 @@ namespace Planer_task_board.App.Service
             {
                 IsSuccess = true,
                 StatusCode = System.Net.HttpStatusCode.OK,
-                Body = nodeLinks
+                Body = nodeLinks?
+                  .Where(x => x != null)
+                  .ToList()!
+               ?? new List<NodeLink>()
             };
         }
 
