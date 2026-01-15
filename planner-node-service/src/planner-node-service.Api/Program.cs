75f8772f06e13a43787bf5e7068636cb85127cf4
pblcenok <pustden524@gmail.com>
2026-01-15 10:41:13 +0500

    фикс

--- planner-node-service/src/planner-node-service.Api/Program.cs
+++ planner-node-service/src/planner-node-service.Api/Program.cs
@@ -38,7 +38,7 @@ void ConfigureServices(IServiceCollection services)
     var createColumnExchange = GetEnvVar("RABBITMQ_CREATE_COLUMN_EXCHANGE");
     var createTaskExchange = GetEnvVar("RABBITMQ_CREATE_TASK_EXCHANGE");
     var contentNodesExchange = GetEnvVar("RABBITMQ_CONTENT_NODES_EXCHANGE");
-    var chatTaskExchange = GetEnvVar("RABBITMQ_CHAT_NODES_EXCHANGE");
+    var chatNodesExchange = GetEnvVar("RABBITMQ_CHAT_NODES_EXCHANGE");
 
     var jwtSecret = GetEnvVar("JWT_AUTH_SECRET");
     var jwtIssuer = GetEnvVar("JWT_AUTH_ISSUER");
