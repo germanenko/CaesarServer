5dc1d0cd5919b3df9891874714bdad753f5ca7a9
pblcenok <pustden524@gmail.com>
2026-03-02 15:59:37 +0500

    сборщик всех микросервисов

---
+++ build.sh
@@ -0,0 +1,8 @@
+./planner-auth-service/build.sh
+./planner-chat-service/build.sh
+./planner-content-service/build.sh
+./planner-node-service/build.sh
+./planner-notify-service/build.sh
+./planner-file-service/build.sh
+./planner-email-service/build.sh
+./planner-mailbox-service/build.sh
