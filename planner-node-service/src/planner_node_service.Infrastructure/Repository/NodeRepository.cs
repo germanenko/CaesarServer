a843ec70303be4b07a13e3583cd936ae3819c453
pblcenok <pustden524@gmail.com>
2026-03-18 14:44:59 +0500

    фикс

--- planner-node-service/src/planner_node_service.Infrastructure/Repository/NodeRepository.cs
+++ planner-node-service/src/planner_node_service.Infrastructure/Repository/NodeRepository.cs
@@ -208,15 +208,19 @@ namespace planner_node_service.Infrastructure.Repository
         public async Task<IEnumerable<Node>?> GetScopes(Guid accountId)
         {
             var syncScopesAccess = await _context.SyncScopeAccess.Where(x => x.AccountId == accountId && x.Permission > Permission.None).ToListAsync();
+            var syncScopesAccessIds = syncScopesAccess.Select(x => x.ScopeId);
 
             var excessScopeIds = new List<Guid>();
 
-            foreach (var scope in syncScopesAccess)
+            foreach (var scopeId in syncScopesAccessIds)
             {
-                if (await CheckScopeAccess(accountId, scope.ScopeId) == false)
+                if (await CheckScopeAccess(accountId, scopeId) == false)
                 {
-                    excessScopeIds.Add(scope.ScopeId);
-                    syncScopesAccess.Remove(scope);
+                    excessScopeIds.Add(scopeId);
+
+                    var excessScope = syncScopesAccess.FirstOrDefault(x => x.ScopeId == scopeId);
+
+                    syncScopesAccess.Remove(excessScope);
                 }
             }
 
