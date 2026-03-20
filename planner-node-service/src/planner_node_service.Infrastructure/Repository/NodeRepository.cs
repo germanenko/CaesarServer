662a7e4267958823386a81de1f0c7474e2a6726c
pblcenok <pustden524@gmail.com>
2026-03-20 18:38:12 +0500

    Update NodeRepository.cs

--- planner-node-service/src/planner_node_service.Infrastructure/Repository/NodeRepository.cs
+++ planner-node-service/src/planner_node_service.Infrastructure/Repository/NodeRepository.cs
@@ -108,7 +108,7 @@ namespace planner_node_service.Infrastructure.Repository
                 Id = nodeBody.Id,
                 Name = nodeBody.Name,
                 Type = nodeBody.Type,
-                CursorId = cursor.Seq
+                Cursor = cursor
             };
 
             if (existingNode == null)
