0f1fb10e197adc9f88597abab5bf253c36dc02f9
pblcenok <pustden524@gmail.com>
2026-03-26 16:36:11 +0500

    миграция userHiddenMessages

---
+++ planner-chat-service/src/planner_chat_service.Api/Migrations/20260326113523_userHiddenMessages.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace planner_chat_service.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class userHiddenMessages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserHiddenMessages",
+                columns: table => new
+                {
+                    MessageId = table.Column<Guid>(type: "uuid", nullable: false),
+                    AccountId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserHiddenMessages", x => new { x.MessageId, x.AccountId });
+                    table.ForeignKey(
+                        name: "FK_UserHiddenMessages_ChatMessages_MessageId",
+                        column: x => x.MessageId,
+                        principalTable: "ChatMessages",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserHiddenMessages");
+        }
+    }
+}
