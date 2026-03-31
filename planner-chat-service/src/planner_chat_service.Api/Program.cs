2e4c6f91900da221a295106390de57862f92bd21
pblcenok <pustden524@gmail.com>
2026-03-31 09:14:19 +0500

    Update Program.cs

--- planner-chat-service/src/planner_chat_service.Api/Program.cs
+++ planner-chat-service/src/planner_chat_service.Api/Program.cs
@@ -19,14 +19,7 @@ using planner_server_package.RabbitMQ;
 using Swashbuckle.AspNetCore.Filters;
 using System.Text;
 
-Env.Load();
-
 var builder = WebApplication.CreateBuilder(args);
-
-builder.WebHost.ConfigureKestrel(options =>
-{
-    options.ListenAnyIP(8090);
-});
 
 ConfigureServices(builder.Services);
 var app = builder.Build();
