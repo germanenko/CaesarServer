a3b4a59a2fa7fd27df9a2e8ec8baccece551abeb
pblcenok <pustden524@gmail.com>
2026-04-10 09:25:56 +0500

    новый способ оптимизации сборки

--- planner-node-service/build.sh
+++ planner-node-service/build.sh
@@ -1,20 +1,2 @@
-#!/bin/bash
-
-set -e
-
-echo "🧹 Cleaning output..."
-rm -rf out
-
-echo "⚙️ Publishing .NET app..."
-
-dotnet publish src/planner_node_service.Api/planner_node_service.Api.csproj \
-  -c Release \
-  -o ./out \
-  /p:UseAppHost=false \
-  /p:DebugType=None \
-  /p:DebugSymbols=false
-
-echo "🐳 Building Docker image..."
+cd planner-node-service
 docker build -t planner-node-service .
-
-echo "✅ Done!"
