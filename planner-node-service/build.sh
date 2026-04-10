a486dcf7283b20e4f8df170965bb6d6a251ca155
pblcenok <pustden524@gmail.com>
2026-04-10 09:18:16 +0500

    новый способ быстрой сборки

--- planner-node-service/build.sh
+++ planner-node-service/build.sh
@@ -1,1 +1,19 @@
-docker build -f dockerfile -t planner-node-service ..
+#!/bin/bash
+
+set -e
+
+echo "🧹 Cleaning output..."
+rm -rf out
+
+echo "⚙️ Publishing .NET app..."
+dotnet publish planner-node-service/src/planner_node_service.Api \
+  -c Release \
+  -o ./out \
+  /p:UseAppHost=false \
+  /p:DebugType=None \
+  /p:DebugSymbols=false
+
+echo "🐳 Building Docker image..."
+docker build -t planner-node-service .
+
+echo "✅ Done!"
