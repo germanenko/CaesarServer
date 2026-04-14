3e4813e27d3207151124852b81b8ee7b77d7a8f8
pblcenok <pustden524@gmail.com>
2026-04-14 16:24:31 +0500

    включил полиморфизм на сваггере

--- planner-content-service/src/planner_content_service.Api/Program.cs
+++ planner-content-service/src/planner_content_service.Api/Program.cs
@@ -214,6 +214,8 @@ void ConfigureSwagger(IServiceCollection services)
 {
     services.AddSwaggerGen(options =>
     {
+        options.UseOneOfForPolymorphism();
+
         options.SwaggerDoc("v1", new OpenApiInfo
         {
             Version = "v1",
