d071d645bcd056a6d1a6504cda55c211d37f4fcc
pblcenok <pustden524@gmail.com>
2026-04-16 09:21:46 +0500

    миграция для внешнего ключа TaskColumn

---
+++ planner-content-service/src/planner_content_service.Api/Migrations/20260416042114_fkForTaskColumn.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace planner_content_service.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class fkForTaskColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_UserTaskColumns_ColumnId",
+                table: "UserTaskColumns",
+                column: "ColumnId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserTaskColumns_Columns_ColumnId",
+                table: "UserTaskColumns",
+                column: "ColumnId",
+                principalTable: "Columns",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserTaskColumns_Columns_ColumnId",
+                table: "UserTaskColumns");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserTaskColumns_ColumnId",
+                table: "UserTaskColumns");
+        }
+    }
+}
