f894a9ddde12710463d67ce8411c7dde7b1b94dc
pblcenok <pustden524@gmail.com>
2026-04-29 10:45:30 +0500

    смена интерфейса

--- planner-client-package/Entities/TaskColumnBody.cs
+++ planner-client-package/Entities/TaskColumnBody.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace planner_client_package.Entities
 {
-    public class TaskColumnBody : ISyncable
+    public class TaskColumnBody : IBody
     {
         public Guid Id { get; set; }
         public Guid? ChatId { get; set; }
